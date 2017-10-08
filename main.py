486cc0fa815622fe4cd5e86b2a32fe023fd80445
Mohamed Kassem <mohamed.samy.k@gmail.com>
2017-10-08 18:43:39 +0200

    overfit

--- main.py
+++ main.py
@@ -114,6 +114,7 @@ def train(epoch):
                 epoch, batch_idx * args.batch_size, len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
             print('Target: ', target)
+            print('Train data: ', data[10, :, 10:14])
 
 def test():
     model.eval()
@@ -139,6 +140,7 @@ def test():
 
 
         test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+        print('nll loss: ', F.nll_loss(output, target, size_average=False), ' .data[0]: ', F.nll_loss(output, target, size_average=False).data[0])
         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
         # while True:
