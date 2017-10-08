dd3695bd458cd7030fb3c30e3b017d8208b0269a
Mohamed Kassem <mohamed.samy.k@gmail.com>
2017-10-08 17:32:29 +0200

    overfit

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ if __name__ == '__main__':
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
-        batch_size=args.batch_size, shuffle=True, drop_last=True, **kwargs)
+        batch_size=args.batch_size, shuffle=False, drop_last=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('./data', train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
